8766d70d0813613cfd56acec629d967cfb2746b2
Mauritz Marais <mauritz303@gmail.com>
2024-05-29 15:37:34 +0200

    added aria expanded attribute toggle for mobile nav

--- src/assets/js/nav.js
+++ src/assets/js/nav.js
@@ -1,7 +1,11 @@
 const navigation = document.querySelector("#cs-navigation");
 const menuToggle = document.querySelector("#cs-navigation .cs-toggle");
+const ul = document.querySelector(".nav-ul");
 
 menuToggle.addEventListener("click", () => {
      menuToggle.classList.toggle("cs-active");
      navigation.classList.toggle("cs-active");
+
+     // set aria expanded attribute
+     ul.getAttribute("aria-expanded") === "true" ? ul.setAttribute("aria-expanded", "false") : ul.setAttribute("aria-expanded", "true");    
 });
